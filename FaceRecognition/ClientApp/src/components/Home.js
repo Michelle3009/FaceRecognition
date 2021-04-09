a6e9d291dbb92a18efec2e576120d1bf1d973ba6
Michelle3009 <michellemolinamoncada@gmail.com>
2021-04-08 21:22:25 -0600

    Menu lateral

--- FaceRecognition/ClientApp/src/components/Home.js
+++ FaceRecognition/ClientApp/src/components/Home.js
@@ -1,60 +1,12 @@
-import React, { Component, useState } from 'react';
-import clsx from 'clsx';
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import List from '@material-ui/core/List';
-import Divider from '@material-ui/core/Divider';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
-import ListItemText from '@material-ui/core/ListItemText';
-import InboxIcon from '@material-ui/icons/MoveToInbox';
-import MailIcon from '@material-ui/icons/Mail';
-import Drawer from '@material-ui/core/Drawer';
+import React, { Component } from 'react';
 
 export class Home extends Component {
     static displayName = Home.name;
-    constructor(props){
-        super(props)
-        this.state = {
-            left: false
-        }
-    }
-    changeState(action) {
-        this.setState({left: action})
-    }
-    toggleDrawer = (open)=>(event)=> {
-        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
-            return;
-        }
-        this.changeState(open);
-
-    };
-    list(anchor) {
-        return(
-        <div
-            role="presentation"
-            onClick={this.toggleDrawer( false)}
-            onKeyDown={this.toggleDrawer( false)}>
-            <List>
-                {['Miembros', 'Parentesco', 'Registro de personas', 'Cerrar sesión'].map((text, index) => (
-                    <ListItem button key={text}>
-                        <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
-                        <ListItemText primary={text} />
-                    </ListItem>
-                ))}
-            </List>
-            <Divider />
-        </div>)
-    }
+    
     render() {
         return (
             <div>
-                <React.Fragment key={"left"} >
-                    <Button onClick={this.toggleDrawer(true)}>Left</Button>
-                    <Drawer anchor={"left"} open={this.state.left} onClose={this.toggleDrawer( false)}>
-                        {this.list("left")}
-                    </Drawer>
-                </React.Fragment>
+                
             </div >
         );
     }
--- FaceRecognition/ClientApp/src/components/NavMenu.js
+++ FaceRecognition/ClientApp/src/components/NavMenu.js
@@ -1,7 +1,17 @@
-import React, { Component } from 'react';
-import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
+import React, { Component} from 'react';
+import { Collapse, Container, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink, Button } from 'reactstrap';
 import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
 import './NavMenu.css';
+import List from '@material-ui/core/List';
+import Divider from '@material-ui/core/Divider';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import MailIcon from '@material-ui/icons/Mail';
+import Drawer from '@material-ui/core/Drawer';
 
 export class NavMenu extends Component {
   static displayName = NavMenu.name;
@@ -11,33 +21,109 @@ export class NavMenu extends Component {
 
     this.toggleNavbar = this.toggleNavbar.bind(this);
     this.state = {
-      collapsed: true
+        collapsed: true, 
+        leftBar: false
+
     };
   }
 
   toggleNavbar () {
     this.setState({
-      collapsed: !this.state.collapsed
+        collapsed: !this.state.collapsed,
+        leftBar: this.state.leftBar
     });
   }
+    changeLeftBar = (state) => {
+        this.setState({
+            collapsed: this.state.collapsed, leftBar: state
+        })
+        
+    }
+    getResponse = (evt) => {
+        this.setState({
+            collapsed: this.state.collapsed, leftBar: false
+        })
+    }
 
   render () {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
-          <Container>
-            <NavbarBrand tag={Link} to="/">Administrador monitoreo </NavbarBrand>
-            <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-            <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-              <ul className="navbar-nav flex-grow">
-                <NavItem>
-                  <NavLink tag={Link} className="text-dark" to="/">Cuenta</NavLink>
-                </NavItem>
-              </ul>
-            </Collapse>
-          </Container>
-        </Navbar>
-      </header>
+      return (
+          <div>
+              {this.state.leftBar && <LeftBar stateLeft={this.changeLeftBar(false)} />}
+          <header>
+            <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" light>
+              <Container>
+                 <NavbarBrand className="navBarBrand" tag={Link} to="/">
+                        <Button color="link" size="lg" onClick={() => this.changeLeftBar(true)}><FontAwesomeIcon icon={faBars} /></Button>Administrador monitoreo </NavbarBrand>
+                <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
+                <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
+                  <ul className="navbar-nav flex-grow">
+                    <NavItem> <NavLink tag={Link} className="text-dark" to="/">Cuenta</NavLink> </NavItem>
+                  </ul>
+                </Collapse>
+              </Container>
+            </Navbar>
+            </header>
+      </div>
     );
   }
 }
+
+
+export class LeftBar extends Component {
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            left:  false
+        }
+    }
+    
+    changeState(action) {
+        this.setState({ left: action })
+        
+        
+    }
+    sendFather = () => {
+        const { stateLeft } = this.props
+        stateLeft();
+    }
+   
+    toggleDrawer = (open) => (event) => {
+        if (event && event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+        this.sendFather();
+        this.changeState(open);
+
+    };
+    list(anchor) {
+        
+        return (
+            <div
+                role="presentation"
+                onClick={this.toggleDrawer(false)}
+                onKeyDown={this.toggleDrawer(false)}>
+                <List>
+                    {['Miembros', 'Parentesco', 'Registro de personas', 'Cerrar sesión'].map((text, index) => (
+                        <ListItem button key={text}>
+                            <ListItemIcon>{index % 2 === 0 ? <InboxIcon /> : <MailIcon />}</ListItemIcon>
+                            <ListItemText primary={text} />
+                        </ListItem>
+                    ))}
+                </List>
+                <Divider />
+            </div>)
+    }
+    render() {
+        return (
+            <div>
+                <React.Fragment key={"left"} >
+                    <Drawer anchor={"left"} open={this.state.left} onClose={this.toggleDrawer(false)} >
+                        {this.list("left")}
+                    </Drawer>
+                </React.Fragment>
+            </div >
+        );
+    }
+}
+
